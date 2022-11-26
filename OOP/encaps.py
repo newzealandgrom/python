1e0bcd42e2767ea85f942d74fa9af16206dcaf09
Vladimir Mikhailov <vladimir@vladimirmikhailov.com>
2022-11-26 15:28:25 +0700

    2

--- OOP/encaps.py
+++ OOP/encaps.py
@@ -11,7 +11,7 @@ class Student:
         """calculate"""
         return sum(self._results) / len(self._years)
     
-    def add_results(self, year, result):
+    def add_result(self, year, result):
         """add result for student"""
         self.years.append(year)
         self._results.append(result)
@@ -21,11 +21,17 @@ class Student:
         result = self._calc_predicted_result()
         print(f'Student {self._name} is predicted: {result}')
 
+
 s = Student('One')
-s.add_result(2020, 66)
-s.add_result(2022, 55)
-s.add_result(2021, 100)
+s.add_result(2020, 88)
+s.add_result(2019, 76)
+s.add_result(2018, 22)
 s.display_predicted_result()
+
+
+
+
+
 
 
 
