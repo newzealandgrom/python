5c9f54255a882b84fa223d09ff3fbd0f80fa1d9e
Vladimir Mikhailov <vladimir@vladimirmikhailov.com>
2022-11-26 19:10:10 +0700

    2

--- OOP/encaps.py
+++ OOP/encaps.py
@@ -21,11 +21,12 @@ class Student:
         print(f'Student {self._name} is predicted: {result}')
 
 
-s = Student('One')
+s = Student('Vladimir')
 s.add_result(2020, 88)
 s.add_result(2019, 76)
 s.add_result(2018, 22)
 s.display_predicted_result()
+
 
 
 
